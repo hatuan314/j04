0a24738cae3e2edae7a6ec9ccff3f1f1fe2b41aa
tuanha <tuanha@acaziasoft.com>
2020-09-05 15:39:29 +0700

    buoi 2

--- j04/src/hatuan314/com/vn/Buoi1/BTVN/Ex1.java
+++ j04/src/hatuan314/com/vn/Buoi1/BTVN/Ex1.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 
 public class Ex1 {
     public static void main(String[] args) {
-        int a;
-        int b;
-        Scanner sc =new Scanner(System.in);
-        System.out.print("a = ");
-        a = sc.nextInt();
-        System.out.print("b = ");
-        b = sc.nextInt();
-        System.out.println("a+b = " + (a+b));
+//        int a;
+//        int b;
+//        Scanner sc =new Scanner(System.in);
+//        System.out.print("a = ");
+//        a = sc.nextInt();
+//        System.out.print("b = ");
+//        b = sc.nextInt();
+//        System.out.println("a+b = " + (a+b));
+        int[] numbersArray = new int[5];
+        numbersArray[0] = 1;
+        numbersArray[0] = 2;
+        for (int i = 0; i < numbersArray.length; i++) {
+            System.out.println(numbersArray[i]);
+        }
     }
 }
